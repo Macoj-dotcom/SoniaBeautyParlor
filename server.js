286da6f5db9805b4ff6294306a37d4e7b7c06636
Giana <ojieoriabure@gmail.com>
2026-06-17 06:38:44 +0900

    added some changes to servejs and packagejson and create vercel

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const uploadRoutes = require("./routes/uploadRouter");
 
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
+
 connectDB();
 const app = express();
 app.use(express.json());
@@ -51,3 +52,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
+
+module.exports = app;
